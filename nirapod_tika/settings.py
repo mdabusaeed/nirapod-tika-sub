41e35d08a4a0c1ead6e4b422592baefcd9d0a48a
mdabusaeed <abu.saeed.nicl@gmail.com>
2025-03-13 14:27:55 +0600

    increse crediantial

--- nirapod_tika/settings.py
+++ nirapod_tika/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
-       "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+       "ACCESS_TOKEN_LIFETIME": timedelta(days=60),
 }
 
 DJOSER = {
