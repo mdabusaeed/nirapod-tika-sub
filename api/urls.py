eb7b1fb98c15787e9e12ffc7e83edb180bfde510
mdabusaeed <abu.saeed.nicl@gmail.com>
2025-04-24 15:01:47 +0600

    Add cart path

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from rest_framework_nested import routers
 from users.views import UserProfileView
 from vaccination.views import VaccineViewSet, VaccinationScheduleViewSet, VaccineReviewViewSet, VaccineCampaignViewSet
 from users.views import UserProfileView,DoctorProfileView
-from order.views import OrderViewSet, CartViewSet
+from order.views import OrderViewSet, CartViewSet, CartItemViewSet    
 
 router = routers.DefaultRouter()
 router.register('patient-profile', UserProfileView, basename='patient-profile')
@@ -15,10 +15,16 @@ router.register('vaccine-campaign', VaccineCampaignViewSet, basename='vaccine-ca
 router.register('order', OrderViewSet, basename='order')
 router.register('cart', CartViewSet, basename='cart')
 
+product_router = routers.NestedDefaultRouter(router, 'vaccine', lookup='vaccine')
+
+carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_router.register('items', CartItemViewSet, basename='cart-item')
 
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(product_router.urls)),
+    path('', include(carts_router.urls)),
     path('auth/', include('djoser.urls')),  
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls')),
